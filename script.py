f799cdecddd3c0bffd995227deda0ccf01fbe0e7
Derek Wan <d.wan@icloud.com>
2025-12-14 08:39:07 +0900

    2025-12-14 08:39:07 (Sun)  > DW-Mac  > derekwan

--- script.py
+++ script.py
@@ -5,6 +5,7 @@
 #   "click",
 #   "dycw-utilities",
 #   "pytest-xdist",
+#   "tomlkit",
 # ]
 # ///
 from __future__ import annotations
@@ -12,7 +13,6 @@ from __future__ import annotations
 from logging import getLogger
 from pathlib import Path
 
-import click
 from click import command, option
 from utilities.click import CONTEXT_SETTINGS_HELP_OPTION_NAMES
 from utilities.logging import basic_config
@@ -22,20 +22,30 @@ _LOGGER = getLogger(__name__)
 
 @command(**CONTEXT_SETTINGS_HELP_OPTION_NAMES)
 @option(
-    "--file",
-    type=click.Path(path_type=Path),
-    default=None,
+    "--pyproject-build-system/--no-pyproject-build-system",
+    default=False,
     show_default=True,
-    help="File to create",
+    help="Add `pyproject.toml` [build-system]",
 )
 @option("--dry-run/--no-dry-run", default=False, show_default=True, help="Dry run")
-def main(*, file: Path | None = None, dry_run: bool = False) -> None:
+def main(*, pyproject_build_system: bool = False, dry_run: bool = False) -> None:
     if dry_run:
         _LOGGER.info("Dry run; exiting...")
         return
     _LOGGER.info("Running...")
-    if file is not None:
-        file.touch()
+    if pyproject_build_system:
+        _add_pyproject_build_system()
+
+
+def _add_pyproject_build_system() -> None:
+    _add_pyproject()
+    _LOGGER.info("Adding `pyproject.toml` [build-system]")
+
+
+def _add_pyproject() -> None:
+    if not (path := Path("pyproject.toml")).is_file():
+        _LOGGER.info("Adding `pyproject.toml`...")
+        path.touch()
 
 
 if __name__ == "__main__":
