dfad11ce2ff45ed362e24f7cadd6ba0c716bf4d4
Derek Wan <d.wan@icloud.com>
2025-12-14 16:46:23 +0900

    2025-12-14 16:46:23 (Sun)  > DW-Mac  > derekwan

--- script.py
+++ script.py
@@ -177,107 +177,111 @@ def _get_table(obj: Container | Table, key: str, /) -> Table:
 
 
 @contextmanager
+def _yield_toml_doc(path: PathLike, desc: str, /) -> Iterator[TOMLDocument]:
+    path = Path(path)
+    doc = _get_doc(path)
+    yield doc
+    if doc != _get_doc(path):
+        _LOGGER.info("Adding '%s' %s...", path, desc)
+        _ = path.write_text(dumps(doc))
+
+
+@contextmanager
 def _yield_pyproject(
     desc: str, /, *, version: str = _SETTINGS.version
 ) -> Iterator[TOMLDocument]:
-    doc = _get_doc(_PYPROJECT_TOML)
-    bld_sys = _get_table(doc, "build-system")
-    bld_sys["build-backend"] = "uv_build"
-    bld_sys["requires"] = ["uv_build"]
-    project = _get_table(doc, "project")
-    project["requires-python"] = f">= {version}"
-    yield doc
-    if doc != _get_doc(_PYPROJECT_TOML):
-        _LOGGER.info("Adding `pyproject.toml` %s...", desc)
-        _ = _PYPROJECT_TOML.write_text(dumps(doc))
+    with _yield_toml_doc(_PYPROJECT_TOML, desc) as doc:
+        bld_sys = _get_table(doc, "build-system")
+        bld_sys["build-backend"] = "uv_build"
+        bld_sys["requires"] = ["uv_build"]
+        project = _get_table(doc, "project")
+        project["requires-python"] = f">= {version}"
+        yield doc
 
 
 @contextmanager
 def _yield_ruff(
     desc: str, /, *, version: str = _SETTINGS.version
 ) -> Iterator[TOMLDocument]:
-    doc = _get_doc(_RUFF_TOML)
-    doc["target-version"] = f"py{version.replace('.', '')}"
-    doc["unsafe-fixes"] = True
-    fmt = _get_table(doc, "format")
-    fmt["preview"] = True
-    fmt["skip-magic-trailing-comma"] = True
-    lint = _get_table(doc, "lint")
-    lint["explicit-preview-rules"] = True
-    fixable = _get_array(lint, "fixable")
-    _ensure_in_array(fixable, "ALL")
-    ignore = _get_array(lint, "ignore")
-    _ensure_in_array(
-        ignore,
-        "ANN401",  # any-type
-        "ASYNC109",  # async-function-with-timeout
-        "C901",  # complex-structure
-        "CPY",  # flake8-copyright
-        "D",  # pydocstyle
-        "E501",  # line-too-long
-        "PD",  # pandas-vet
-        "PERF203",  # try-except-in-loop
-        "PLC0415",  # import-outside-top-level
-        "PLE1205",  # logging-too-many-args
-        "PLR0904",  # too-many-public-methods
-        "PLR0911",  # too-many-return-statements
-        "PLR0912",  # too-many-branches
-        "PLR0913",  # too-many-arguments
-        "PLR0915",  # too-many-statements
-        "PLR2004",  # magic-value-comparison
-        "PT012",  # pytest-raises-with-multiple-statements
-        "PT013",  # pytest-incorrect-pytest-import
-        "PYI041",  # redundant-numeric-union
-        "S202",  # tarfile-unsafe-members
-        "S310",  # suspicious-url-open-usage
-        "S311",  # suspicious-non-cryptographic-random-usage
-        "S602",  # subprocess-popen-with-shell-equals-true
-        "S603",  # subprocess-without-shell-equals-true
-        "S607",  # start-process-with-partial-path
-        # preview
-        "S101",  # assert
-        # formatter
-        "W191",  # tab-indentation
-        "E111",  # indentation-with-invalid-multiple
-        "E114",  # indentation-with-invalid-multiple-comment
-        "E117",  # over-indented
-        "COM812",  # missing-trailing-comma
-        "COM819",  # prohibited-trailing-comma
-        "ISC001",  # single-line-implicit-string-concatenation
-        "ISC002",  # multi-line-implicit-string-concatenation
-    )
-    _ensure_not_in_array(
-        ignore,
-        "RUF022",  # unsorted-dunder-all
-        "RUF029",  # unused-async
-        "S101",  # assert
-        "SLF001",  # private-member-access
-    )
-    lint["preview"] = True
-    select = _get_array(lint, "select")
-    _ensure_in_array(
-        select,
-        "ALL",
-        "RUF022",  # unsorted-dunder-all
-        "RUF029",  # unused-async
-    )
-    extend_ignores = _get_table(lint, "extend-per-file-ignores")
-    test_py = _get_array(extend_ignores, "test_*.py")
-    _ensure_in_array(
-        test_py,
-        "S101",  # assert
-        "SLF001",  # private-member-access
-    )
-    bugbear = _get_table(lint, "flake8-bugbear")
-    bugbear["ban-relative-imports"] = "all"
-    isort = _get_table(lint, "isort")
-    req_imps = _get_array(isort, "required-imports")
-    _ensure_in_array(req_imps, "from __future__ import annotations")
-    isort["split-on-trailing-comma"] = False
-    yield doc
-    if doc != _get_doc(_RUFF_TOML):
-        _LOGGER.info("Adding `ruff.toml` %s...", desc)
-        _ = _RUFF_TOML.write_text(dumps(doc))
+    with _yield_toml_doc(_RUFF_TOML, desc) as doc:
+        doc["target-version"] = f"py{version.replace('.', '')}"
+        doc["unsafe-fixes"] = True
+        fmt = _get_table(doc, "format")
+        fmt["preview"] = True
+        fmt["skip-magic-trailing-comma"] = True
+        lint = _get_table(doc, "lint")
+        lint["explicit-preview-rules"] = True
+        fixable = _get_array(lint, "fixable")
+        _ensure_in_array(fixable, "ALL")
+        ignore = _get_array(lint, "ignore")
+        _ensure_in_array(
+            ignore,
+            "ANN401",  # any-type
+            "ASYNC109",  # async-function-with-timeout
+            "C901",  # complex-structure
+            "CPY",  # flake8-copyright
+            "D",  # pydocstyle
+            "E501",  # line-too-long
+            "PD",  # pandas-vet
+            "PERF203",  # try-except-in-loop
+            "PLC0415",  # import-outside-top-level
+            "PLE1205",  # logging-too-many-args
+            "PLR0904",  # too-many-public-methods
+            "PLR0911",  # too-many-return-statements
+            "PLR0912",  # too-many-branches
+            "PLR0913",  # too-many-arguments
+            "PLR0915",  # too-many-statements
+            "PLR2004",  # magic-value-comparison
+            "PT012",  # pytest-raises-with-multiple-statements
+            "PT013",  # pytest-incorrect-pytest-import
+            "PYI041",  # redundant-numeric-union
+            "S202",  # tarfile-unsafe-members
+            "S310",  # suspicious-url-open-usage
+            "S311",  # suspicious-non-cryptographic-random-usage
+            "S602",  # subprocess-popen-with-shell-equals-true
+            "S603",  # subprocess-without-shell-equals-true
+            "S607",  # start-process-with-partial-path
+            # preview
+            "S101",  # assert
+            # formatter
+            "W191",  # tab-indentation
+            "E111",  # indentation-with-invalid-multiple
+            "E114",  # indentation-with-invalid-multiple-comment
+            "E117",  # over-indented
+            "COM812",  # missing-trailing-comma
+            "COM819",  # prohibited-trailing-comma
+            "ISC001",  # single-line-implicit-string-concatenation
+            "ISC002",  # multi-line-implicit-string-concatenation
+        )
+        _ensure_not_in_array(
+            ignore,
+            "RUF022",  # unsorted-dunder-all
+            "RUF029",  # unused-async
+            "S101",  # assert
+            "SLF001",  # private-member-access
+        )
+        lint["preview"] = True
+        select = _get_array(lint, "select")
+        _ensure_in_array(
+            select,
+            "ALL",
+            "RUF022",  # unsorted-dunder-all
+            "RUF029",  # unused-async
+        )
+        extend_ignores = _get_table(lint, "extend-per-file-ignores")
+        test_py = _get_array(extend_ignores, "test_*.py")
+        _ensure_in_array(
+            test_py,
+            "S101",  # assert
+            "SLF001",  # private-member-access
+        )
+        bugbear = _get_table(lint, "flake8-bugbear")
+        bugbear["ban-relative-imports"] = "all"
+        isort = _get_table(lint, "isort")
+        req_imps = _get_array(isort, "required-imports")
+        _ensure_in_array(req_imps, "from __future__ import annotations")
+        isort["split-on-trailing-comma"] = False
+        yield doc
 
 
 if __name__ == "__main__":
