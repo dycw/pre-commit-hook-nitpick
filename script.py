9a564f3b405c7502d92b1229c6b2b51759f771e3
Derek Wan <d.wan@icloud.com>
2026-01-01 11:54:15 +0900

    2026-01-01 11:54:15 (Thu)  > DW-Mac  > derekwan

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S uv run --script
 # /// script
-# requires-python = ">=3.14"
+# requires-python = ">=3.12"
 # dependencies = [
 #   "click >=8.3.1, <8.4",
 #   "dycw-utilities >=0.173.0, <0.174",  # 0.175.10 does not work
