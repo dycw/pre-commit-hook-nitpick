1867686d753baff0e594cf9a77dce37a92fef60b
Derek Wan <d.wan@icloud.com>
2026-01-01 11:26:23 +0900

    2026-01-01 11:26:23 (Thu)  > DW-Mac  > derekwan

--- script.py
+++ script.py
@@ -5,7 +5,7 @@
 #   "click >=8.3.1, <8.4",
 #   "dycw-utilities >=0.175.10, <0.176",
 #   "rich >=14.2.0, <14.3",
-#   "ruamel-yaml >=0.19.0, <0.20",
+#   "ruamel-yaml >=0.18.17, <0.19",
 #   "tomlkit >=0.13.3, <0.14",
 #   "typed-settings[attrs, click] >=25.3.0, <25.4",
 #   "xdg-base-dirs >=6.0.2, <6.1",
