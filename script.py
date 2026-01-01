d6c266bf98397af06ef8edabc8de93a28a91378d
Derek Wan <d.wan@icloud.com>
2026-01-01 11:17:14 +0900

    2026-01-01 11:17:14 (Thu)  > DW-Mac  > derekwan

--- script.py
+++ script.py
@@ -175,7 +175,7 @@ class Settings:
     python_version: str = option(default="3.14", help="Python version")
     readme: bool = option(default=False, help="Set up 'README.md'")
     repo_name: str | None = option(default=None, help="Repo name")
-    ruff: bool = option(default=True, help="Set up 'ruff.toml'")
+    ruff: bool = option(default=False, help="Set up 'ruff.toml'")
     script: str | None = option(
         default=None, help="Set up a script instead of a package"
     )
